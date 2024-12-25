0dbacb4d6a082f49896821968df47a485f3640b2
epixenpai <142733889+epixenpai@users.noreply.github.com>
2024-12-25 10:39:47 +0530

    Update manifest.js

--- src/manifest.js
+++ src/manifest.js
@@ -1,8 +1,8 @@
 const manifest = {
     id: 'org.tamilmovies',
     version: '1.0.0',
-    name: 'Tamil Movies',
-    description: 'High-rated and trending Tamil movies from various OTT platforms',
+    name: 'Tamil & English Movies',
+    description: 'High-rated and trending Tamil and English movies from various OTT platforms',
     types: ['movie'],
     background: 'https://www.pngarts.com/files/2/Blue-Abstract-Lines-PNG-Transparent-Image.png',
     catalogs: [
@@ -22,6 +22,36 @@ const manifest = {
             type: 'movie',
             id: 'tamil.recent',
             name: 'Recent Tamil Movies',
+            extra: [{ name: 'genre' }, { name: 'skip' }]
+        },
+        {
+            type: 'movie',
+            id: 'english.mustwatch',
+            name: 'Must-Watch English Movies',
+            extra: [{ name: 'genre' }, { name: 'skip' }]
+        },
+        {
+            type: 'movie',
+            id: 'english.highgrossing',
+            name: 'High-Grossing English Movies',
+            extra: [{ name: 'genre' }, { name: 'skip' }]
+        },
+        {
+            type: 'movie',
+            id: 'english.newott',
+            name: 'New OTT English Releases',
+            extra: [{ name: 'genre' }, { name: 'skip' }]
+        },
+        {
+            type: 'movie',
+            id: 'english.intheatre',
+            name: 'English Movies in Theatres',
+            extra: [{ name: 'genre' }, { name: 'skip' }]
+        },
+        {
+            type: 'movie',
+            id: 'english.scifi',
+            name: 'English Sci-Fi Movies',
             extra: [{ name: 'genre' }, { name: 'skip' }]
         }
     ],
